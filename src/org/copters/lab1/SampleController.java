0b338d465bda7d365154785b105f87a6f4df7a37
Dmitry Polchinsky <89823187178@yandex.ru>
2021-02-25 13:38:25 +0300

    Add basic infrastructure

---
+++ src/org/copters/lab1/SampleController.java
@@ -0,0 +1,12 @@
+package org.copters.lab1;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
+public class SampleController {
+    public Label helloWorld;
+
+    public void sayHelloWorld(ActionEvent actionEvent) {
+        helloWorld.setText("Hello World!");
+    }
+}
