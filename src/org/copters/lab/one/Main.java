b367a57eecddd859ad6c9fa03705137d0d76ac2a
Dmitry Polchinsky <89823187178@yandex.ru>
2021-03-04 10:56:56 +0300

    Hide gui stuff for now

--- src/org/copters/lab/one/Main.java
+++ src/org/copters/lab/one/Main.java
@@ -1,26 +1,12 @@
 package org.copters.lab.one;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
 import org.copters.lab.one.minimizer.DichotomyMinimizer;
 import org.copters.lab.one.minimizer.FibonacciMinimizer;
 import org.copters.lab.one.minimizer.Minimizer;
 import org.copters.lab.one.util.Segment;
 import org.copters.lab.one.util.UnimodalFunction;
 
-public class Main extends Application {
-
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
-
+public class Main {
     private static final UnimodalFunction FUNCTION =
             x -> 0.2 * x * Math.log(x) + (x - 2.3) * (x - 2.3);
 
@@ -38,6 +24,5 @@ public class Main extends Application {
     public static void main(String[] args) {
         run(new DichotomyMinimizer(SEGMENT, EPS));
         run(new FibonacciMinimizer(SEGMENT, EPS));
-        launch(args);
     }
 }
