304addb5e558eeb963ecde6565162759dac65efb
Dmitry Polchinsky <89823187178@yandex.ru>
2021-03-05 14:07:48 +0300

    Add memorization for parabolic

--- src/org/copters/lab/one/minimizer/ParabolicMinimizer.java
+++ src/org/copters/lab/one/minimizer/ParabolicMinimizer.java
@@ -5,9 +5,15 @@ import org.copters.lab.one.util.Segment;
 import org.copters.lab.one.util.UnimodalFunction;
 
 public class ParabolicMinimizer extends AbstractMinimizer {
+    private static final Random RANDOM = new Random();
+
     private double x1;
     private double x2;
     private double x3;
+
+    private double f1;
+    private double f2;
+    private double f3;
 
     public ParabolicMinimizer(Segment segment, double epsilon) {
         super(segment, epsilon);
@@ -20,26 +26,31 @@ public class ParabolicMinimizer extends AbstractMinimizer {
 
     @Override
     protected Segment next(UnimodalFunction function) {
-        double f1 = function.applyAsDouble(x1);
-        double f2 = function.applyAsDouble(x2);
-        double f3 = function.applyAsDouble(x3);
-
         double a1 = (f2 - f1) / (x2 - x1);
         double a2 = ((f3 - f1) / (x3 - x1) - (f2 - f1) / (x2 - x1)) / (x3 - x2);
 
         double x = 0.5 * (x1 + x2 - a1 / a2);
         double f = function.applyAsDouble(x);
 
-        if (x1 < x  && x < x2 && x2 < x3 && f >= f2) {
-            x1 = x;
-        } else if (x1 < x && x < x2 && x2 < x3 && f < f2) {
-            x3 = x2;
-            x2 = x;
-        } else if (x1 < x2 && x2 < x && x < x3 && f <= f2) {
-            x1 = x2;
-            x2 = x;
-        } else if (x1 < x2 && x2 < x && x < x3 && f > f2) {
-            x3 = x;
+        if (f >= f2) {
+            if (x < x2) {
+                x1 = x;
+                f1 = f;
+            } else {
+                x3 = x;
+                f3 = f;
+            }
+        } else {
+            if (x < x2) {
+                x3 = x2;
+                x2 = x;
+                f3 = f2;
+            } else {
+                x1 = x2;
+                f1 = f2;
+                x2 = x;
+            }
+            f2 = f;
         }
 
         return new Segment(x1, x3);
@@ -56,13 +67,12 @@ public class ParabolicMinimizer extends AbstractMinimizer {
         x2 = (segment.getFrom() + segment.getTo()) / 2.;
         x3 = segment.getTo();
 
-        Random random = new Random();
-        double f1 = function.applyAsDouble(x1);
-        double f2 = function.applyAsDouble(x2);
-        double f3 = function.applyAsDouble(x3);
+        f1 = function.applyAsDouble(x1);
+        f2 = function.applyAsDouble(x2);
+        f3 = function.applyAsDouble(x3);
 
         while (!(f1 >= f2 && f2 <= f3)) {
-            x2 = x1 + (x3 - x1) * random.nextDouble();
+            x2 = x1 + (x3 - x1) * RANDOM.nextDouble();
             f2 = function.applyAsDouble(x2);
         }
     }
