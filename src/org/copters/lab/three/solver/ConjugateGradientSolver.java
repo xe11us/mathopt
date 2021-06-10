4b637951a5755b99cb8c977c3cc30ced30214040
Dmitry Polchinsky <89823187178@yandex.ru>
2021-06-10 23:58:54 +0300

    Fix conjugate solver

--- src/org/copters/lab/three/solver/ConjugateGradientSolver.java
+++ src/org/copters/lab/three/solver/ConjugateGradientSolver.java
@@ -4,30 +4,32 @@ import org.copters.lab.three.util.SquareMatrix;
 import org.copters.lab.three.util.Vector;
 
 public class ConjugateGradientSolver implements Solver {
-    private final double EPS = 1e-7;
-    private final int ITERATIONS = 100000;
+    private static final double EPS = 1e-7;
+    private static final int ITERATIONS = 100000;
     private Vector xPrev;
 
-    public ConjugateGradientSolver(Vector x0) {
+    public ConjugateGradientSolver(final Vector x0) {
         this.xPrev = x0;
     }
 
     @Override
-    public Vector solve(SquareMatrix matrix, Vector values) {
+    public Vector solve(final SquareMatrix matrix, final Vector values) {
         Vector rPrev = values.subtract(matrix.multiply(xPrev));
         Vector zPrev = rPrev;
+        Vector x = xPrev;
+
         double rSqr = rPrev.dot(rPrev);
 
         int iteration = 0;
-        while (rPrev.length() / values.length() > EPS && iteration++ < ITERATIONS) {
-            double alpha = rSqr / matrix.multiply(zPrev).dot(zPrev);
-            xPrev = xPrev.add(zPrev.multiply(alpha));
+        while (rPrev.length() > EPS * values.length() && iteration++ < ITERATIONS) {
+            final double alpha = rSqr / matrix.multiply(zPrev).dot(zPrev);
+            x = x.add(zPrev.multiply(alpha));
             rPrev = rPrev.subtract(matrix.multiply(zPrev).multiply(alpha));
-            double beta = rPrev.dot(rPrev) / rSqr;
+            final double beta = rPrev.dot(rPrev) / rSqr;
             zPrev = rPrev.add(zPrev.multiply(beta));
             rSqr = rPrev.dot(rPrev);
         }
 
-        return xPrev;
+        return x;
     }
 }
