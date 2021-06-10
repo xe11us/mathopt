d877c611e2c24fba321689096ef43ec92c8ff4c4
Alexey Vasilyev <vasilyev-av22@mail.ru>
2021-06-10 19:54:33 +0300

    Fix Marquardt second method

--- src/org/copters/lab/four/minimizer/marquardt/MarquardtSecond.java
+++ src/org/copters/lab/four/minimizer/marquardt/MarquardtSecond.java
@@ -6,8 +6,7 @@ import org.copters.lab.four.util.Gradient;
 import org.copters.lab.four.util.HessianMatrix;
 import org.copters.lab.three.solver.ConjugateGradientSolver;
 import org.copters.lab.three.solver.Solver;
-import org.copters.lab.three.util.SquareMatrix;
-import org.copters.lab.three.util.Vector;
+import org.copters.lab.three.util.*;
 
 public class MarquardtSecond implements MultiMinimizer {
     private final double epsilon;
@@ -20,16 +19,21 @@ public class MarquardtSecond implements MultiMinimizer {
 
     private boolean isPositive(SquareMatrix matrix) {
         double[][] answer = new double[matrix.size()][matrix.size()];
-        for (int i = 0; i < matrix.size(); i++) {
-            for (int j = 0; j < i; j++) {
+
+        for (int i = 1; i < matrix.size(); i++) {
+            for (int j = 1; j < i; j++) {
                 double sum = 0;
-                for (int p = 0; p < i; p++) {
-                    sum += matrix.get(i, p) * matrix.get(j, p);
+                for (int p = 1; p < i; p++) {
+                    sum += answer[i][p] * answer[j][p];
+                }
+                if (Math.abs(answer[j][j]) < epsilon) {
+                    return false;
                 }
                 answer[i][j] = (matrix.get(i, j) - sum) / answer[j][j];
             }
+
             double sum = 0;
-            for (int p = 0; p < i; p++) {
+            for (int p = 1; p < i; p++) {
                 sum += answer[i][p] * answer[i][p];
             }
             sum = matrix.get(i, i) - sum;
